6d6f848310e00afd82c457310f5cea6105d480a7
Adelmonte <mihaimuntean21@gmail.com>
2021-02-18 13:46:20 +0200

    Added moveCursor(int x, int y) function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void moveCursor(int x, int  y);
+
 int main()
 {
+	system("clear");
+	moveCursor(15,5);
 	printf("Hello world!\n");
 	return 0;
+}
+
+void moveCursor(int x, int y)
+{
+	printf("%c[%d;%df",0x1b,x,y);
 }
