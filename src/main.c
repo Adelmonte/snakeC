fd6afd560457c0c4f8cd67abd272e846b2a5a5f3
Adelmonte <mihaimuntean21@gmail.com>
2021-02-25 13:16:14 +0200

    Added kbHit() and kbHit2()

--- src/main.c
+++ src/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <fcntl.h>
 
 //console: 36 l; 100 c
 
@@ -17,32 +22,41 @@
 #define FOOD (char) 'X'
 #define BLANK (char) ' '
 
+#define SNAKE_MAX_LENGTH (int) 5
 
 void  moveCursor(int x, int y);
 void  drawCanvas(int x, int y);
 char  kbPressed();
 int* pickFoodLocation(int x, int y);
 int* updateHeadPosition(int x, int y);
-void snake();
+void startSnakeGame();
 void clrscr();
 void print(int x, int y);
+int kbHit();
+int kbHit2();
 
 int main()
 {
 	system("clear");
-	snake();
+	startSnakeGame();
 	return 0;
 }
 
-// snake()
+// startSnakeGame()
 //
 // Parameters:
 // Return:
-void snake()
+void startSnakeGame()
 {
-	int sLength[100];
-	int x;
-	int y;
+	int length = 3; // initial snake length
+
+	int historyX[SNAKE_MAX_LENGTH]; // for saving the x coordinates of the snake body
+	int historyY[SNAKE_MAX_LENGTH]; // for saving the y coordinates of the snake body
+		
+	int canvasH = 36;
+	int canvasW = 100;
+	drawCanvas(canvasW,canvasH); // after this call the gameSpace will be canvasH-2, canvasW-2
+
 	int position[] = {0,0};
 	int* pPosition;
 	pPosition=&position[0];
@@ -65,6 +79,18 @@ void snake()
 	drawCanvas(36,100); // enables a console of that size
 	 
 } 
+
+// drawCanvas(int x, int y);
+// Draw a border on the limit of the canvas.
+// Parameters: int x -> width of the canvas
+// 		int y-> height of the canvas
+// Return: void
+void drawCanvas(int x, int y)
+{
+	// to be filled
+}
+
+
 
 void print(int x, int y)
 {
@@ -103,6 +129,59 @@ int* pickFoodLocation(int x, int y)
 	return p;
 }
 
+// kbHit()
+// Verifies if a key was pressed upon calling.
+// Code from : https://cboard.cprogramming.com/c-programming/63166-kbhit-linux.html
+// Paraneters: none
+// Return:int 1 -> if was pressed a key on keyboard
+//		0 -> if not
+int kbHit()
+{
+	struct termios oldt, newt;
+	int ch;
+	int oldf;
+	int r;
+
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+	ch = getchar();
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+	if (ch !=EOF)
+	{
+		ungetc(ch, stdin);
+		r=1;
+	}
+	else r=0;
+	return r;
+	
+
+}
+
+// kbHit2()
+int kbHit2()
+{
+	struct timeval tv;
+	fd_set rdfs;
+
+	tv.tv_sec=0;
+	tv.tv_usec=0;
+
+	FD_ZERO(&rdfs);
+	FD_SET (STDIN_FILENO, &rdfs);
+
+	select(STDIN_FILENO+1, &rdfs, NULL, NULL, &tv);
+	return FD_ISSET(STDIN_FILENO, &rdfs);
+}
+
+
 // kbPressed()
 // Sense if a key on the keyboard was pressed
 // Parameters: none
@@ -111,18 +190,8 @@ char kbPressed()
 {
 	char c=NULL;
 	scanf("%c",c); // maybe it won't work because scanf block the thread of execution
-		       // until it's pressed a key and not listen in the event of a key pressed
+		       // until it's pressed a key and is not listening in the event of a pressed ke
 	return c;
-}
-
-// drawCanvas(int x, int y);
-//
-//
-//
-//
-void drawCanvas(int x, int y)
-{
-	
 }
 
 // moveCursor(int x, int y);
